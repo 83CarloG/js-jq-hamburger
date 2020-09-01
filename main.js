e0a66020f354538cac2fd5ffcd732022da6ba022
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-09-01 17:03:58 +0200

    Final

---
+++ main.js
@@ -0,0 +1,20 @@
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+// (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+// Partite come base del vostro esercizio scaricando questa repo:
+// https://bitbucket.org/booleancareers/ex-hamburger-menu-base/downloads
+// Ricordate di scrivere solo il codice JavaScript con jQuery e non modificare HTML e CSS
+
+var hamburgerYes = $('.fas.fa-bars');
+var hamburgerNo = $('.fas.fa-times')
+var visibile = false;
+
+hamburgerYes.click(function () {
+	$('.hamburger-menu').css('display', 'block');
+}
+);
+
+hamburgerNo.click(function () {
+	$('.hamburger-menu').css('display', 'none');
+}
+);
